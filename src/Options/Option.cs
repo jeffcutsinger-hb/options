c5edbb4addcb2be3e65ff68219030b5de2d737f3
Jeff Cutsinger (Healthbox) <jeff.cutsinger@healthboxit.com>
2014-04-14 08:04:07 -0500

    Added implicit conversion operators to Option<TOption>.

--- src/Options/Option.cs
+++ src/Options/Option.cs
@@ -107,5 +107,14 @@ namespace Options
 			}
 			return new Option<TOption>(value);
 		}
+
+        /// <summary>
+        /// Returns a value that will be implicitly coerced into the appropriate <see cref="Option{TOption}"/> type.
+        /// </summary>
+        /// <returns>A value that will be implicitly coerced into the appropriate <see cref="Option{TOption}"/> type.</returns>
+	    public static OptionNone None()
+	    {
+	        return new OptionNone();
+	    }
 	}
 }
